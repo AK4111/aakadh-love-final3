93e096b1d794cec148e8e96b1d9ac525e8cc9c9f
AK4111 <98637357+AK4111@users.noreply.github.com>
2022-07-27 21:02:17 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -669,7 +669,7 @@ async def auto_filter(client, msg, spoll=False):
             [
                 InlineKeyboardButton(
                     text=f"[{get_size(file.file_size)}] {file.file_name}", 
-                    url=await get_shortlink(f"https://Telegram.me/{temp.U_NAME}?start=pre_{file.file_id}")
+                    url=await get_shortlink(f"https://api.shareus.in/shortLink?token={API}&format=json&link=https://telegram.dog/{temp.U_NAME}?start=pre_{file.file_id}").json()
                 ),
             ]
             for file in files
@@ -679,7 +679,7 @@ async def auto_filter(client, msg, spoll=False):
             [
                 InlineKeyboardButton(
                     text=f"{file.file_name}",
-                    url=await get_shortlink(f"https://Telegram.me/{temp.U_NAME}?start=pre_{file.file_id}")
+                    url=await get_shortlink(f"https://api.shareus.in/shortLink?token={API}&format=json&link=https://telegram.dog/{temp.U_NAME}?start=pre_{file.file_id}").json()
                 ),
                 InlineKeyboardButton(
                     text=f"{get_size(file.file_size)}",
